184fd2c594ea33ee993bbca178a6be1bedbdaa48
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-09 17:37:06 +0530

    created public and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import {  Switch } from "react-router";
 import 'rsuite/dist/styles/rsuite-default.css';
+
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+
 import './styles/main.scss';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <PublicRoute path="/signin"><SignIn/></PublicRoute>
+      <PrivateRoute path="/"><Home/></PrivateRoute>
+    </Switch>
   );
 }
 
