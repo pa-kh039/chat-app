d21754a887aae91b04de406590d6c780dbf71bd2
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-09 15:12:58 +0530

    initialised firebase project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,15 @@
+
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAEYuqzMngwLsh05h8ZmkZ5dotmagANMkI",
+    authDomain: "chat-web-app-98de3.firebaseapp.com",
+    databaseURL: "https://chat-web-app-98de3-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-98de3",
+    storageBucket: "chat-web-app-98de3.appspot.com",
+    messagingSenderId: "928995862655",
+    appId: "1:928995862655:web:c225fa3a206f43bd97b74a"
+  };
+// eslint-disable-next-line
+  const app = firebase.initializeApp(firebaseConfig);
+  
