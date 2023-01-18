2f9d83b82c6ef952ffd41eda89b2f191c8a3c66c
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-18 12:36:37 +0530

    online-offline status stored in firebase

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react'
 import { Alert, Button, Drawer, Icon } from 'rsuite'
 import Dashboard from '.';
+import { isOfflineForDatabase } from '../../context/profile.context';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 
 const DashboardToggle = () => {
 
@@ -11,9 +12,15 @@ const DashboardToggle = () => {
   
 
   const onSignOut = useCallback(()=>{
-        auth.signOut();
-        Alert.info('Signed Out !',2000);
-        close();
+
+    database.ref(`/status/${auth.currentUser.uid}`).set(isOfflineForDatabase).then(()=>{
+      
+      auth.signOut();
+      Alert.info('Signed Out !',2000);
+      close();
+
+    }).catch(err=>Alert.error(err.message,2000))
+
   },[close]);
 
   return (
