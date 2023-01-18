ddd7ceace24f4b535d9b6f9a4dc1ace612fd1716
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-18 22:10:31 +0530

    added options to grant and revoke admin permissions

--- src/components/chatwindow/messages/index.jsx
+++ src/components/chatwindow/messages/index.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router';
+import { Alert } from 'rsuite';
 import { database } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
@@ -20,14 +21,38 @@ const Messages = () => {
     return ()=>{
       messagesRef.off('value');
     }
-  }, [chatId])
+  }, [chatId]);
+
+  const handleAdmin = useCallback(
+    async (uid) => {
+      const adminsRef = database.ref(`/rooms/${chatId}/admins`);
+      let alertMsg;
+      await adminsRef.transaction(admins => {
+        if (admins) {
+          if (admins[uid]) {
+            // eslint-disable-next-line
+            admins[uid]=null;
+            alertMsg="admin permission removed";
+          } else {
+            // eslint-disable-next-line
+            admins[uid]=true;
+            alertMsg="admin permission granted";
+          }
+        }
+        return admins;
+      });
+      Alert.info(alertMsg,4000);
+    },
+    [chatId],
+  )
+  
   
   return (
     <ul className='msg-list custom-scroll'>
       {isChatEmpty && <li>No Messages yet..
         </li>}
         {
-          canShowMessages && messages.map(msg=>< MessageItem key={msg.id} message={msg }/>)
+          canShowMessages && messages.map(msg=>< MessageItem key={msg.id} message={msg } handleAdmin={handleAdmin}/>)
         }
     </ul>
   )
