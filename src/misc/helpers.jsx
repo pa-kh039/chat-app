16cfa61cadd44b0cae3bd419703344f4eba6abd7
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-23 10:46:48 +0530

    grouped messages as per date

--- src/misc/helpers.jsx
+++ src/misc/helpers.jsx
@@ -39,3 +39,18 @@ export async function getUserUpdates(userId, keyToUpdate , value, db){
 
     return updates;
 }
+
+// groupBy(messages,(msgItem)=>msgItem.createdAt)
+export function groupBy(array,groupingKeyFn)
+{
+    return array.reduce((result,item)=>{
+        const groupingKey = groupingKeyFn(item);
+        if (!result[groupingKey])
+        {
+            // eslint-disable-next-line no-param-reassign
+            result[groupingKey]=[];
+        }
+        result[groupingKey].push(item);
+        return result;
+    },{});
+}
